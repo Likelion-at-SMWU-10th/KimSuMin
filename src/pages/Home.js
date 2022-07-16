255f0f223b38558ce9625eb96da8645b48c4e6fc
sumin <tnals8939@daum.net>
2022-07-16 16:40:30 +0900

    final assignment

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const home = () => {
+    return (
+        <div>
+            {/*개인 페이지 제목*/}
+            <h1>여름에는 털옷을 안입는 바야바</h1>
+
+            {/* 김수민의 개인페이지 입니다. */}
+            <p> 김수민의 개인 페이지 입니다 !! </p>
+        </div>
+    )
+}
+
+export default home;
---
+++ src/pages/WhoAmI.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const whoAmI = () => {
+    return (
+        <div>
+            <h1>자기소개</h1>
+            {}
+            <ol>
+                <li>이름 : 김수민</li>
+                <li>취미 : 하늘에서 뛰어내리기 (like 패러글라이딩?) </li>
+                <li>최근의 관심사 : 온통 eng...</li>
+            </ol>
+        </div>
+    );
+}
+
+export default whoAmI;
